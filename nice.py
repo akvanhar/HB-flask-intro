4899866dbf87d15a6fa5e103854c6b218db05f07
Hackbright Student <info@hackbrightacademy.com>
2015-07-16 13:02:19 -0700

    Webpage can both insult and compliment user. Uses python function to generate HTML.

--- nice.py
+++ nice.py
@@ -10,7 +10,19 @@ app = Flask(__name__)
 # route to handle the landing page of a website.
 @app.route('/')
 def start_here():
-    return "Hi! This is the home page."
+    return """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Webpage of Compliments</title>
+        </head>
+        <body>
+            <h1>Hi! This is the home page.</h1>
+            <a href="/hello">Take me to the 'hello' page!</a>
+        </body>
+    </html>
+    """
+
 
 # route to display a simple web page
 @app.route('/hello')
@@ -23,8 +35,34 @@ def say_hello():
         </head>
         <body>
             <h1>Hi There!</h1>
-            <form action="/greet">
+            <form action="/greet" method='post'>
                 <label>What's your name? <input type="text" name="person"></label>
+                <label>
+                    Choose the compliment you would like to receive!
+                <select name="compliment">
+                    <option value="awesome">Awesome</option>
+                    <option value="terrific">Terrific</option>
+                    <option value="fantastic">Fantastic</option>
+                    <option value="neato">Neato</option>
+                    <option value="wowza">Wowza</option>
+
+                </select>
+                </label>
+                <input type="submit">
+            </form>
+
+            <form action="/diss">
+                <label>What's your name? <input type="text" name="person"></label>
+                <label>
+                    Choose the insult you would prefer to receive!
+                <select name="insult">
+                    <option value="smelly">Smelly</option>
+                    <option value="terrible">Terrible</option>
+                    <option value="poopy">Poopy</option>
+                    <option value="gross">Gross</option>
+                    <option value="bleh">Bleh</option>
+                </select>
+                </label>
                 <input type="submit">
             </form>
         </body>
@@ -32,27 +70,38 @@ def say_hello():
 
     """
 
-@app.route('/greet')
+@app.route('/greet', methods=['POST'])
 def greet_person():
+    player = request.form.get("person")
+    compchoice = request.form.get("compliment")
+
+    #AWESOMENESS = [
+    #    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
+    #    'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+
+   # compliment = choice(AWESOMENESS)
+
+    return html_gen("Compliment", player, compchoice)
+
+@app.route('/diss')
+def insult_person():
     player = request.args.get("person")
+    insult = request.args.get("insult")
 
-    AWESOMENESS = [
-        'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza', 'oh-so-not-meh',
-        'brilliant', 'ducky', 'coolio', 'incredible', 'wonderful', 'smashing', 'lovely']
+    return html_gen("Insult", player, insult)
 
-    compliment = choice(AWESOMENESS)
-
-    return """
+def html_gen(descriptor, player, input_str):
+    return_html = """
     <!DOCTYPE html>
     <html>
         <head>
-            <title>A Compliment</title>
+            <title>%s for you</title>
         </head>
         <body>
             Hi %s I think you're %s!
         </body>
-    </html>""" % (player, compliment)
-
+    </html>""" % (descriptor, player, input_str)
+    return return_html
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our web app
